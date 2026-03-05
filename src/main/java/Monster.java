5066b18719de49bf18ebdf696266ef939c0f1747
Tyler Fleming <tfleming@csumb.edu>
2026-03-04 19:08:23 -0800

    monster class

--- src/main/java/Monster.java
+++ src/main/java/Monster.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Monster {
+    private final Integer hp;
+    private final Integer xp;
+    private final Integer maxHP;
+    private HashMap<String, Integer> items;
+
+
+    public Monster(Integer maxHP, Integer xp, HashMap<String, Integer> items){
+        this.maxHP = maxHP;
+        this.hp = this.maxHP;
+        this.xp = xp;
+        this.items = items;
+    }
+
+    @Override
+    public String toString(){
+        return "hp=" + hp + "/" + maxHP;
+    }
 }
